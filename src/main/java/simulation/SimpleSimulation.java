83bd8bb3032258e60d5b267aac0ede004768efa2
Shinnosuke Wanaka <wanaka@is.k.u-tokyo.ac.jp>
2017-05-04 11:05:47 -0400

    chage the function of file output to solve the situation where output file is empty
    Signed-off-by: Shinnosuke Wanaka <wanaka@is.k.u-tokyo.ac.jp>

--- src/main/java/simulation/SimpleSimulation.java
+++ src/main/java/simulation/SimpleSimulation.java
@@ -1,5 +1,6 @@
 package simulation;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ import model.Ship;
 public class SimpleSimulation extends Simulation{
 	
 	private final String OUTPUT_DIR = "./data/";
+	private final String OUTPUT_FILE = "result.xlsx";
 
 	public SimpleSimulation(int endTime) {
 		super(endTime);
@@ -45,26 +47,37 @@ public class SimpleSimulation extends Simulation{
 		outputList.add(String.valueOf(freight));
 		double fuelPrice = Market.getFuels().get(0).getPrice();
 		outputList.add(String.valueOf(fuelPrice));
-		
+
 		try{
-			FileInputStream filein = new FileInputStream(OUTPUT_DIR + "result.xlsx");
-			Workbook wb = WorkbookFactory.create(filein);
-			String safeName = WorkbookUtil.createSafeSheetName("reseult");
-            Sheet sheet1 = wb.getSheet(safeName);
+			Workbook wb = null;
+			Sheet sheet = null;
+			if(new File(OUTPUT_DIR+OUTPUT_FILE).exists()){
+				FileInputStream filein = new FileInputStream(OUTPUT_DIR + OUTPUT_FILE);
+				wb = WorkbookFactory.create(filein);
+				String safeName = WorkbookUtil.createSafeSheetName("reseult");
+	            sheet = wb.getSheet(safeName);
+				filein.close();
+			}else{
+				wb = new XSSFWorkbook();
+				String safeName = WorkbookUtil.createSafeSheetName("reseult");
+				sheet = wb.createSheet(safeName);
+			}
+			
+			
             
             //CreationHelper createHelper = wb.getCreationHelper();
             
             //Rows(行にあたる)を作る。Rowsは0始まり。
-            Row row = sheet1.createRow(now);
+            Row row = sheet.createRow(now);
             //cell(列にあたる)を作って、そこに値を入れる。
             int i = 0;
             for (String elem : outputList){
             	row.createCell(i).setCellValue(elem);
             	i++;
             }
-            FileOutputStream fileOut = new FileOutputStream(OUTPUT_DIR + "result.xlsx");
+            FileOutputStream fileOut = new FileOutputStream(OUTPUT_DIR + OUTPUT_FILE);
             wb.write(fileOut);
-            filein.close();
+          
 			fileOut.close();
 		}catch (Exception e){
 			e.printStackTrace();
