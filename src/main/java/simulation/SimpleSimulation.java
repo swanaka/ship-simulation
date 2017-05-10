aadd49448b59db5324204c49e3a0e9753a68d5b5
Koichiro Shiraishi <ko5shira1982@gmail.com>
2017-05-10 17:30:00 -0400

    Change output file format from Excel to CSV

--- src/main/java/simulation/SimpleSimulation.java
+++ src/main/java/simulation/SimpleSimulation.java
@@ -1,30 +1,24 @@
 package simulation;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CreationHelper;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.ss.util.WorkbookUtil;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import model.Fleet;
 import model.Market;
-import model.Port;
-import model.PortNetwork;
 import model.Ship;
 
 public class SimpleSimulation extends Simulation{
-	
+
 	private final String OUTPUT_DIR = "./data/";
 	private final String OUTPUT_FILE = "result.xlsx";
+	private final String OUTPUT_CSV_FILE = "result.csv";
+	// ファイルの初期化、追加入力の管理のためのフラッグ　KS
+	private boolean file_flag = false;
 
 	public SimpleSimulation(int endTime) {
 		super(endTime);
@@ -47,7 +41,7 @@ public class SimpleSimulation extends Simulation{
 		outputList.add(String.valueOf(freight));
 		double fuelPrice = Market.getFuels().get(0).getPrice();
 		outputList.add(String.valueOf(fuelPrice));
-
+/*
 		try{
 			Workbook wb = null;
 			Sheet sheet = null;
@@ -62,11 +56,11 @@ public class SimpleSimulation extends Simulation{
 				String safeName = WorkbookUtil.createSafeSheetName("reseult");
 				sheet = wb.createSheet(safeName);
 			}
-			
-			
-            
+
+
+
             //CreationHelper createHelper = wb.getCreationHelper();
-            
+
             //Rows(行にあたる)を作る。Rowsは0始まり。
             Row row = sheet.createRow(now);
             //cell(列にあたる)を作って、そこに値を入れる。
@@ -77,17 +71,47 @@ public class SimpleSimulation extends Simulation{
             }
             FileOutputStream fileOut = new FileOutputStream(OUTPUT_DIR + OUTPUT_FILE);
             wb.write(fileOut);
-          
+
 			fileOut.close();
 		}catch (Exception e){
 			e.printStackTrace();
 		}finally{
-			
-			
+
 		}
-		
+*/
+		// CSVファイルで計算結果を出力するように変更　KS
+		File file = new File(OUTPUT_DIR+OUTPUT_CSV_FILE);
+		PrintWriter pw = null;
+
+		try {
+			// 出力ストリームを生成
+			if(file_flag == false){
+				pw = new PrintWriter(
+						new OutputStreamWriter(
+							new FileOutputStream(file, false)));
+				file_flag = true;
+			}else{
+				pw = new PrintWriter(
+						new OutputStreamWriter(
+							new FileOutputStream(file, true)));
+			}
+			String sample = null;
+			for (String outputPerList: outputList){
+				sample = outputPerList + ", ";
+				pw.print(sample);
+			}
+			pw.println();
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			// クローズ処理
+			if (pw != null) {
+				pw.close();
+			}
+		}
+
 		System.out.println("Now: " + now + " Saved!");
-		
+
 	}
 
 }
